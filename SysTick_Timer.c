861af6b0d6e330d316f8f383d2d9e9eba28c0325
MahMoudMostaAfa <mahmoud2030m@gmail.com>
2024-05-07 14:57:27 +0300

    systick modified

--- SysTick_Timer.c
+++ SysTick_Timer.c
@@ -1,40 +1,26 @@
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
-
+#include "SysTick_Timer.h"
 void SysTick_Init() // To initialize SysTick Counter
 {
-	NVIC_ST_CTLR_R = 0;    //disable SysTick during setup
-	NVIC_ST_RELOAD_R = 1;  // just intial val for the RELOAD register
+	NVIC_ST_CTRL_R = 0;    //disable SysTick during setup
+	NVIC_ST_RELOAD_R = 0X00FFFFFF;  // just intial val for the RELOAD register
 	NVIC_ST_CURRENT_R = 0; // to assure that any Curren val clear
-	NVIC_ST_CTLR_R = 0X05; // Enable the SysTick with no intrrupt
+	NVIC_ST_CTRL_R = 0X05; // Enable the SysTick with no intrrupt
 }
 
-void SysTick_wait_1ms() // for 1 milli second Delay
-{
-	SysTick_Init();
-	NVIC_ST_RELOAD_R = 16000 - 1; // (16MHz * 10^-3(ms)) -1
-	while((NVIC_ST_CTLR_R & 0x00010000) == 0){}; // wait
-	}
-
-void SysTick_wait_1sec()
-{
-	SysTick_Init();
-	NVIC_ST_RELOAD_R = 16,000,000 - 1;(16MHz * 1sec) -1
-	while((NVIC_ST_CTLR_R & 0x00010000) == 0){}; // wait
-	}
-
-	void general_delay_ms(uint32_t num_of_ms) // for a delay that is multiple of 1ms
-	{
-		uint32_t i;
-		for(i=0;i<num_of_ms;i++)
-				SysTick_wait_1ms();
-	}
+void systick_wait_1ms(){
+	NVIC_ST_CTRL_R=0;// CLEAR  ENABLE BIT
+	NVIC_ST_RELOAD_R=16000-1 ; //  (1ms*SYS_CLK)-1
+	NVIC_ST_CURRENT_R=0; //CLEAR CURRENT
+	NVIC_ST_CTRL_R=0x05; // enable , inten ,clk_src ;
+	while( (NVIC_ST_CTRL_R & 0x00010000) == 0) ; // make sure flag not set to 1 
 	
-	void general_delay_sec(uint32_t num_of_sec) // for a delay that is multiple of 1sec
-	{
-		uint32_t i;
-		for(i=0;i<num_of_sec;i++)
-				SysTick_wait_1sec();
+}
+
+void wait_ms(unsigned int time){
+	unsigned int i;
+	for(i=0 ;i<time ; i++ ){
+		systick_wait_1ms();
 	}
-		
-		
+}

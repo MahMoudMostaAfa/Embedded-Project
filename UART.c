6d951a7dc618cbf2c038a24d7d2ce92100ad3889
Marwan ELNabawy <117053465+6marwan6@users.noreply.github.com>
2024-05-17 05:07:42 +0300

    comments for UART.c

--- UART.c
+++ UART.c
@@ -36,7 +36,7 @@ void UART_0_Write_string(char* byte){
 }
 
 
-// initialze UART 1
+// initialze UART 5
 void UART_5_Init(void){
 	SYSCTL_RCGCUART_R|=0x0020;//active uart5
 	SYSCTL_RCGCGPIO_R|=0x0010; ;// active port E 
@@ -46,18 +46,18 @@ void UART_5_Init(void){
 	UART5_FBRD_R=11 ;  // int(.16*64 +.5)
 	UART5_LCRH_R= 0x0070 ;// 8-bit word length ,enable fifo
 	UART5_CTL_R =0x0301; //enable uart
-	GPIO_PORTE_AFSEL_R|=0x30; //enable alt function PA0,PA1
+	GPIO_PORTE_AFSEL_R|=0x30; //enable alt function PE$,PE%
 	GPIO_PORTE_PCTL_R |=  0x00011000; // configure uart for PE4 ,PE5
 	GPIO_PORTE_DEN_R |=0x30;// enable digital
 	GPIO_PORTE_AMSEL_R%=~0x30; //disble analog
 	
 }
-// Write data for UART 1
+// Write data for UART 5
 void UART_5_Write(char data) {
 	while ((UART5_FR_R & 0x0020)!=0) ; 	// check if fifo is full
 	UART5_DR_R = data ;
 }
-// read data for UART 1
+// read data for UART 5
 char UART_5_Read(void){
 	while ((UART5_FR_R & 0x0010)!=0);	// check if fifo is empty
 	return (char) (UART5_DR_R & 0xFF);
